048def951a650c6cbe329814f00c6d1f48e1f57d
Sujin Kim <gfsusan@naver.com>
2018-11-14 19:44:47 +0900

    Edited gyro code

--- sy/gyro/gyro.ino
+++ sy/gyro/gyro.ino
@@ -1,117 +1,39 @@
-// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class
-// 10/7/2011 by Jeff Rowberg <jeff@rowberg.net>
-// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
-//
-// Changelog:
-//      2013-05-08 - added multiple output formats
-//                 - added seamless Fastwire support
-//      2011-10-07 - initial release
-
-/* ============================================
-I2Cdev device library code is placed under the MIT license
-Copyright (c) 2011 Jeff Rowberg
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-===============================================
-*/
-
-// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
-#include "I2Cdev.h"
-#include "MPU6050.h"
-#include <SoftwareSerial.h>
-#include <string.h>
+#include <Wire.h>
+const int MPU = 0x68;               //MPU6050 I2C주소
+int AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+void get6050();
  
-SoftwareSerial BTSerial(2,3);
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
-#endif
-
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for InvenSense evaluation board)
-// AD0 high = 0x69
-MPU6050 accelgyro;
-//MPU6050 accelgyro(0x69); // <-- use for AD0 high
-
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
-
-
-// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
-// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
-// not so easy to parse, and slow(er) over UART.
-#define OUTPUT_READABLE_ACCELGYRO
-
-// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
-// binary, one right after the other. This is very fast (as fast as possible
-// without compression or data loss), and easy to parse, but impossible to read
-// for a human.
-//#define OUTPUT_BINARY_ACCELGYRO
-
-
-bool blinkState = false;
-int toothNum = 0;
-char sep[10] = "\r\n";
-
-void setup() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
-
-    Serial.begin(9600);
-    BTSerial.begin(9600);//블루투스와의 통신속도 설정
-    
+void setup()
+{
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);                    //MPU6050 을 동작 대기 모드로 변경
+  Wire.endTransmission(true);
+  Serial.begin(9600);
 }
-
-void loop() {
-    // read raw accel/gyro measurements from device
-    toothNum = (toothNum++ % 7) + 1;
-    
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    if(az>0x0){
-//        BTSerial.write("11\r\n");
-        char whole[10] = "1";
-        char num[10] = {'0'};
-        itoa(toothNum, &num[0], 10);
-        strcat(whole, num);
-        strcat(whole, sep);
-        BTSerial.write(whole);
-        Serial.println(whole);
-        delay(2000);
-    }
-    else if(az<=0x0){
-//      BTSerial.write("41\r\n");
-      char whole[10] = "4";
-      char num[10] = {'0'};
-      itoa(toothNum, &num[0], 10);
-      strcat(whole, num);
-      strcat(whole, sep);
-      BTSerial.write(whole);
-      Serial.println(whole);
-      delay(2000);
-      }
-
+void loop()
+{
+  get6050();//센서값 갱신
+  //받아온 센서값을 출력합니다.
+  Serial.print(AcX);
+  Serial.print("\t");
+  Serial.print(AcY);
+  Serial.print("\t");
+  Serial.print(AcZ);
+  Serial.println();
+  delay(500);
+}
+void get6050(){
+  Wire.beginTransmission(MPU);        //MPU6050 호출
+  Wire.write(0x3B);//AcX 레지스터 위치 요청
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 14, true);    //14byte의 데이터를 요청
+  AcX = Wire.read() << 8|Wire.read(); //두개의 나뉘어진 바이트를 하나로 이어붙입니다.
+  AcY = Wire.read() << 8|Wire.read();
+  AcZ = Wire.read() << 8|Wire.read();
+  Tmp = Wire.read() << 8|Wire.read();
+  GyX = Wire.read() << 8|Wire.read();
+  GyY = Wire.read() << 8|Wire.read();
+  GyZ = Wire.read() << 8|Wire.read();
 }
