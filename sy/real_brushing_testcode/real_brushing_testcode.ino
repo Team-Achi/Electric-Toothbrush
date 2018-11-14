b2c2ac95ae30c85cfe4c512d8a922f9a1b11f1c9
Sujin Kim <gfsusan@naver.com>
2018-11-14 20:34:01 +0900

    Modified testcode

--- sy/real_brushing_testcode/real_brushing_testcode.ino
+++ sy/real_brushing_testcode/real_brushing_testcode.ino
@@ -23,6 +23,7 @@ int buttonApin = 9;
 int brush[100]    = { 11, 21, 31, 41, 11, 12, 13, 14, 15, 16, 17, 16, 15, 14, 13, 12, 11, 21, 22, 23, 24, 25, 26, 27, 26, 25, 24, 23, 22, 21, 31, 32, 33, 34, 35, 36, 37, 36, 35, 34, 33, 32, 31, 41, 42, 43, 44, 45, 46, 47, 46, 45, 44, 43, 42, 41 };
 int duration[100] = {  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1 }; 
 int brushCounter = 0;
+int total = 0;
 
 // Bluetooth write interval
 int interval = 1000;
@@ -33,6 +34,10 @@ void setup() {
 
     Serial.begin(9600);
     BTSerial.begin(9600);//블루투스와의 통신속도 설정
+
+    for (int i = 0; i < 100; i++) {
+      total += duration[i];
+    }
 }
 
 void loop() {
@@ -46,7 +51,11 @@ void loop() {
   }
   // pressure
   pressureReading = analogRead(pressurePin);  
- 
+
+  // do nothing if end of array 
+  if (brushCounter > total)
+    return;
+    
   Serial.print("Analog reading = ");
   Serial.print(pressureReading);     // the raw analog reading
 
